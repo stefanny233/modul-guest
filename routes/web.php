2a71ff14c343ef7046dce94caa727a4d65008264
Stefanny Pop <stefanny@gmail.com>
2025-09-26 18:20:48 +0700

    membuat penduduk

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PerangkatController;
+use App\Http\Controllers\PendudukController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/perangkat', [PerangkatController::class, 'index']);
+Route::get('/penduduk', [PendudukController::class, 'index']);
