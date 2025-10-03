a54e7fb1a3f731ad878ce67e8ae575abf1ae6da5
Stefanny Pop <stefanny@gmail.com>
2025-10-03 13:02:04 +0700

    membuat file baru penduduk

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GuestController;
 use App\Http\Controllers\PendudukController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/guest', [GuestController::class, 'index']);
 
 Route::get('/penduduk', [PendudukController::class, 'index']);
