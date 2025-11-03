23d532f1638b5282157daeb8e4783379bc2fac03
Stefanny Pop <stefanny24si@mahasiswa.pcr.ac.id>
2025-11-03 18:50:58 +0700

    perbaikan baru lagi

--- resources/views/pages/perangkat_desa/create.blade.php
+++ resources/views/pages/perangkat_desa/create.blade.php
@@ -27,10 +27,6 @@
                         <form action="{{ route('perangkat_desa.store') }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             <div class="row g-3">
-                                {{-- <div class="col-12">
-                                    <label>Warga ID</label>
-                                    <input type="text" name="warga_id" class="form-control" required>
-                                </div> --}}
 
                                 <div class="col-12">
                                     <label>Jabatan</label>
